01b702a7bf7d30a3b590af12ca8a422b9911c589
wgp-2020 <70625361+wgp-2020@users.noreply.github.com>
2022-05-15 21:39:15 +0800

    Add files via upload

---
+++ web.sh
@@ -0,0 +1,32 @@
+curl -Ok https://raw.githubusercontent.com/wgp-2020/AX/main/web && chmod +x ./web
+
+echo '{
+    "log": {
+        "loglevel": "warning"
+    },
+    "inbounds": [
+        {
+            "port": '$PORT',
+            "protocol": "'`[ $type ] && echo "$type" || echo "trojan" `'",
+            "settings": {
+                "clients": [
+                    {
+                        "'` [ $type = trojan ] && echo "password" || echo "id" `'": "'`[ $id ] && echo "$id" || echo "1eb6e917-774b-4a84-aff6-b058577c60a5" `'"
+                    }
+                ],
+                "decryption": "none"
+            },
+            "streamSettings": {
+                "network": "ws",
+                "security": "none"
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
+}' > config.json
+
+./web -config=config.json
