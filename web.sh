954026fec6ce1b1a8a5fee877551192ac5746f39
wgp-2020 <70625361+wgp-2020@users.noreply.github.com>
2022-05-15 21:54:53 +0800

    Update web.sh

--- web.sh
+++ web.sh
@@ -11,7 +11,7 @@ echo '{
             "settings": {
                 "clients": [
                     {
-                        "'` [ $type = trojan ] && echo "password" || echo "id" `'": "'`[ $id ] && echo "$id" || echo "1eb6e917-774b-4a84-aff6-b058577c60a5" `'"
+                        "'` [[ $type = trojan || ! $type ]] && echo "password" || echo "id" `'": "'`[ $id ] && echo "$id" || echo "1eb6e917-774b-4a84-aff6-b058577c60a5" `'"
                     }
                 ],
                 "decryption": "none"
